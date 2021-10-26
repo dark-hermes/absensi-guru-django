c501325afa0c429d6d608d0c11e04b29d60b3916
Fanreza <rezaramdhani461@gmail.com>
2021-10-26 22:53:14 +0700

    100% responsive slider mobike

--- static/js/script.js
+++ static/js/script.js
@@ -1,60 +1,58 @@
-var serverClock = jQuery("#jamServer");
-if (serverClock.length > 0) {
-    showServerTime(serverClock, serverClock.text());
+function inputCheck(){
+    var checked = $('input[type=radio]:checked')
+    $('input[type=radio]')
+        .click(function() {
+        checked.filter(':not(:checked)').trigger('deselect');
+        checked = $('input[type=radio]:checked')
+    })
+
+
+    document.getElementById('inlineRadio4').addEventListener('click', function() {
+        document.getElementById('text').removeAttribute("disabled");
+    });
+
+    $('#inlineRadio4').bind('deselect', function () {
+        document.getElementById('text').setAttribute("disabled","");
+    })  
+
+    document.getElementById('inlineRadio8').addEventListener('click', function() {
+        document.getElementById('text2').removeAttribute("disabled");
+    });
+
+    $('#inlineRadio8').bind('deselect', function () {
+        document.getElementById('text2').setAttribute("disabled","");
+    });
 }
 
-function showServerTime(obj, time) {
-    var parts   = time.split(":"),
-        newTime = new Date();
 
-    newTime.setHours(parseInt(parts[0], 10));
-    newTime.setMinutes(parseInt(parts[1], 10));
-    newTime.setSeconds(parseInt(parts[2], 10));
+function responsiveSlider(){
+    width = screen.width;
+    height = screen.height;
+    slider = document.getElementsByClassName("slider");
 
-    var timeDifference  = new Date().getTime() - newTime.getTime();
+    (function($) {
+        let $window = $(window)
 
-    var methods = { 
-        displayTime: function () {
-            var now = new Date(new Date().getTime() - timeDifference);
-            obj.text([
-                methods.leadZeros(now.getHours(), 2),
-                methods.leadZeros(now.getMinutes(), 2),
-                methods.leadZeros(now.getSeconds(), 2)
-            ].join(":"));
-            setTimeout(methods.displayTime, 500);
-        },
+        function resize() {
+            if ($window.width() >= 300 && $window.width() <= 760) {
+                for (let i = 0; i < slider.length; i++) {
+                  slider[i].classList.add("carousel-item")
+                }
 
-        leadZeros: function (time, width) {
-            while (String(time).length < width) {
-                time = "0" + time;
+                slider[0].parentElement.parentElement.classList.add("mobile")
+                slider[0].parentElement.parentElement.classList.remove("dekstop")
             }
-            return time;
+            else{
+                for (let i = 0; i < slider.length; i++) {
+                  slider[i].classList.remove("carousel-item")
+                }
+
+                slider[0].parentElement.parentElement.classList.remove("mobile")
+                slider[0].parentElement.parentElement.classList.add("dekstop")
+            }
         }
-    }
-    methods.displayTime();
+        $window.resize(resize).trigger('resize');
+    })(jQuery);
 }
 
 
-var checked = $('input[type=radio]:checked')
-$('input[type=radio]')
-    .click(function() {
-    checked.filter(':not(:checked)').trigger('deselect');
-    checked = $('input[type=radio]:checked')
-})
-
-
-document.getElementById('inlineRadio4').addEventListener('click', function() {
-    document.getElementById('text').removeAttribute("disabled");
-});
-
-$('#inlineRadio4').bind('deselect', function () {
-    document.getElementById('text').setAttribute("disabled","");
-})  
-
-document.getElementById('inlineRadio8').addEventListener('click', function() {
-    document.getElementById('text2').removeAttribute("disabled");
-});
-
-$('#inlineRadio8').bind('deselect', function () {
-    document.getElementById('text2').setAttribute("disabled","");
-});
