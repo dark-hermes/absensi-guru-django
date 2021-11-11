1ae86fd446501482272d162f6e1228b911757bb6
Fanreza <rezaramdhani461@gmail.com>
2021-11-11 17:57:20 +0700

    Add date for parameter

--- static/js/vueApp.js
+++ static/js/vueApp.js
@@ -3,15 +3,28 @@ function showDataAbsen(){
         el: '#table-pdf',
         delimiters: ['[[', ']]'],
         data: {
-            dataAbsen: ''
+            dataAbsen: '',
+            dateNow: '',
         },
 
         mounted(){
-            let url = 'https://127.0.0.1:8000/api/showabsen/'
+            let url = 'https://localhost:8000/api/showabsen/'
             fetch(url)
                 .then(response => response.json())
                 .then(data =>{
                     this.dataAbsen = data
+
+                    // Split data date
+                    $.each(data,function(index, value){
+                        splitDate = (new Date(value.presence_date));
+                        this.month = splitDate.getMonth()+1;
+                        this.year = splitDate.getFullYear()
+                    });
+
+                    // Split date now
+                    let today = new Date();
+                    this.dateNow = today.getFullYear()+'-'+(today.getMonth()+2)+'-'+today.getDate();
+                    console.log(this.dataAbsen.length)
                 })
         }
 
