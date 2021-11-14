93a4275051b9bee7876ab582dc25e48581bfb470
Fanreza <rezaramdhani461@gmail.com>
2021-11-14 18:59:02 +0700

    filter mapel

--- static/js/vueApp.js
+++ static/js/vueApp.js
@@ -4,29 +4,75 @@ function showDataAbsen(){
         delimiters: ['[[', ']]'],
         data: {
             dataAbsen: '',
-            dateNow: '',
+            dateNow:'',
         },
 
         mounted(){
-            let url = 'https://localhost:8000/api/showabsen/'
+            let url = 'https://localhost:8000/api/showabsen/';
             fetch(url)
-                .then(response => response.json())
+                .then(response => response.json() )
                 .then(data =>{
-                    this.dataAbsen = data
+                    this.dataAbsen = data;
 
                     // Split data date
                     $.each(data,function(index, value){
                         splitDate = (new Date(value.presence_date));
                         this.month = splitDate.getMonth()+1;
-                        this.year = splitDate.getFullYear()
+                        this.year = splitDate.getFullYear();
                     });
 
                     // Split date now
                     let today = new Date();
-                    this.dateNow = today.getFullYear()+'-'+(today.getMonth()+2)+'-'+today.getDate();
-                    console.log(this.dataAbsen.length)
-                })
-        }
+                    this.dateNow = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+                });
+        },
 
+        // methods:{
+        //     filterToday(){
+        //         let pastDates = this.dataAbsen.filter(x => Date.parse(x.presence_date) < this.dateNow);
+        //         console.log(pastDates.presence_date)
+        //         this.dataAbsen = pastDates
+        //     }
+        // },
+
+    });
+};
+
+
+
+function showMapel(){
+    var app = new Vue({
+        el: '#mapel-form',
+        delimiters: ['[[', ']]'],
+        data: {
+            dataKelas:'',
+            parseData:'',
+        },
+
+        mounted(){
+            let url = 'https://localhost:8000/api/subjects/';
+            fetch(url)
+                .then(response => response.json() )
+                .then(data =>{this.dataKelas = data;
+            });
+        },
+
+        methods:{
+           onChange(event) {
+              value = event.target.value;
+
+              if (value == 2) {
+                    let produktif = this.dataKelas.filter(x => x.category == 2);
+                    this.dataKelas = produktif;
+              };
+
+              else if (value == 1) {
+                    let mapelWajib = this.dataKelas.filter(x => x.category == 1);
+                    this.dataKelas = mapelWajib;
+              };
+
+              
+            };
+        },
     });
 };
