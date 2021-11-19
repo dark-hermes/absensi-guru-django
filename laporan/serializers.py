a4bc10723a233e990fa978cadaa2486adc86f04a
Hermawan Sentyaki <52445704+dark-hermes@users.noreply.github.com>
2021-11-19 08:09:33 +0700

    Update api serializer

--- laporan/serializers.py
+++ laporan/serializers.py
@@ -1,16 +1,25 @@
 from rest_framework import serializers
-from laporan.models import SubjectName, StudyReport, GuidanceReport, ScientificWorkReport, InnovativeWorkReport, HumanDevelopmentReport, DutyReport
+from laporan.models import ClassName, SubjectName, StudyReport, GuidanceReport, ScientificWorkReport, InnovativeWorkReport, HumanDevelopmentReport, DutyReport
 
+class ClassSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ClassName
+        fields = '__all__'
 class SubjectSerializers(serializers.ModelSerializer):
     class Meta:
         model = SubjectName
-        fields = '__all__'
-        
-        
+        fields = '__all__' 
 class StudyReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudyReport
         fields = '__all__'
+        
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['class_name'] = ClassSerializer(instance.class_name).data
+        data['subject_name'] = SubjectSerializers(instance.subject_name).data
+        return data
+    
 class GuidanceReportSerializers(serializers.ModelSerializer):
     class Meta:
         model = GuidanceReport
