3ecfa737c262bbc66baa9adb9be9c6c2fc6c048f
kyuubang <bayhaqisptr04@gmail.com>
2021-12-06 07:50:02 +0700

    Use Environment Variable

--- absensi_guru/settings.py
+++ absensi_guru/settings.py
@@ -13,23 +13,30 @@ from sys import platform
 from pathlib import Path
 from os.path import abspath
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = 'django-insecure-#alkzuck5xssxo_7*xphj0(tn$9!va7!^)z92yxr#h6-h8n94v'
+SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-#alkzuck5xssxo_7*xphj0(tn$9!va7!^)z92yxr#h6-h8n94v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = int(os.environ.get('DEBUG', default=0))
-DEBUG = True
+DEBUG = env('DEBUG')
+#DEBUG = True
 
-# ALLOWED_HOSTS = ['localhost', '10.5.0.4', '*']
+ALLOWED_HOSTS = ['localhost', '10.5.0.4', '192.168.100.120', '0.0.0.0', '127.0.0.1']
 # SECURE_SSL_REDIRECT = int(os.environ.get('SECURE_SSL_REDIRECT'))
 SECURE_SSL_REDIRECT = True
 
@@ -90,27 +97,27 @@ WSGI_APPLICATION = 'absensi_guru.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'absen_db',
-#         'USER': 'absen',
-#         'PASSWORD': 'root',
-#         'HOST': '10.5.0.2',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_absen',
-        'USER': 'postgres',
+        'NAME': 'absen_db',
+        'USER': 'absen',
         'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': ''
+        'HOST': '10.5.0.2',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'db_absen',
+#         'USER': 'postgres',
+#         'PASSWORD': 'root',
+#         'HOST': 'localhost',
+#         'PORT': ''
+#     }
+# }
 
 
 # Password validation
@@ -139,6 +146,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+BASE_URL = absen.smkn1cibinong.sch.id
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Apply database migrations ..."
+echo "Apply database migrations.."
 python manage.py migrate
 
 echo "Collect static files.."
@@ -9,7 +9,7 @@ python manage.py collectstatic --noinput
 echo "Create crontab job.."
 python manage.py crontab add
 
-echo "Starting Server ..."
+echo "Starting Server.."
 gunicorn absensi_guru.wsgi:application --bind 0.0.0.0:8000
 
 
