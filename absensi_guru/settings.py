e808a2c5e2f049fa4316e6dd8285221a37ccc0f4
kyuubang <bayhaqisptr04@gmail.com>
2021-12-06 17:33:05 +0700

    commented BASE_URL

--- absensi_guru/settings.py
+++ absensi_guru/settings.py
@@ -146,7 +146,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-BASE_URL = absen.smkn1cibinong.sch.id
+#BASE_URL = absen.smkn1cibinong.sch.id
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
