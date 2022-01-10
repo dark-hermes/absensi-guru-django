5162c6619c54172cef2c597e598eb8cd6f90ca9b
Hermawan Sentyaki <52445704+dark-hermes@users.noreply.github.com>
2022-01-10 20:59:50 +0700

    Reformat API

--- absen/serializers.py
+++ absen/serializers.py
@@ -1,28 +1,57 @@
 from rest_framework import serializers
 from absen.models import Presence, CheckoutRecord, CheckinRecord
 from userauth.models import Days, Employee
+from django.contrib.auth import get_user_model
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = '__all__'
 class PresenceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Presence
         fields = '__all__'
+        
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['employee_id'] = EmployeeSerializer(instance.employee_id).data
+        return data
         
 class CheckoutSerializer(serializers.ModelSerializer):
     class Meta:
         model = CheckoutRecord
         fields = '__all__'
         
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['employee_id'] = EmployeeSerializer(instance.employee_id).data
+        return data
+        
 class CheckinSerializer(serializers.ModelSerializer):
     class Meta:
         model = CheckinRecord
         fields = '__all__'
         
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['employee_id'] = EmployeeSerializer(instance.employee_id).data
+        return data
+        
 class DaysSerializer(serializers.ModelSerializer):
     class Meta:
         model = Days
         fields = '__all__'
-        
+    
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['employee_id'] = EmployeeSerializer(instance.employee_id).data
+        return data
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
         fields = '__all__'
+        
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['user'] = UserSerializer(instance.user).data
+        return data
--- laporan/serializers.py
+++ laporan/serializers.py
@@ -1,14 +1,25 @@
 from rest_framework import serializers
-from laporan.models import ClassName, SubjectName, StudyReport, GuidanceReport, ScientificWorkReport, InnovativeWorkReport, HumanDevelopmentReport, DutyReport
-
+from laporan.models import ClassName, SubjectName, StudyReport, GuidanceReport, ScientificWorkReport, InnovativeWorkReport, HumanDevelopmentReport, DutyReport, SubjectCategory, ScientificWorkCategory, InnovativeWorkCategory, MajorRole
+from absen.serializers import EmployeeSerializer
 class ClassSerializer(serializers.ModelSerializer):
     class Meta:
         model = ClassName
+        fields = '__all__'
+        
+class SubjectCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubjectCategory
         fields = '__all__'
 class SubjectSerializers(serializers.ModelSerializer):
     class Meta:
         model = SubjectName
         fields = '__all__' 
+        
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret['class_name'] = ClassSerializer(instance.class_name).data
+        ret['category'] = SubjectCategorySerializer(instance.category).data
+        return ret
 class StudyReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudyReport
@@ -25,22 +36,64 @@ class GuidanceReportSerializers(serializers.ModelSerializer):
         model = GuidanceReport
         fields = '__all__'
         
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['employee_id'] = EmployeeSerializer(instance.employee_id).data
+        return data
+        
+class ScientificWorkCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ScientificWorkCategory
+        fields = '__all__'
+        
+
 class ScientificWorkReportSerializers(serializers.ModelSerializer):
     class Meta:
         model = ScientificWorkReport
         fields = '__all__'
         
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['employee_id'] = EmployeeSerializer(instance.employee_id).data
+        data['category'] = ScientificWorkCategorySerializer(instance.category).data
+        return data
+        
+class InnovativeWorkCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InnovativeWorkCategory
+        fields = '__all__'
 class InnovativeWorkReportSerializers(serializers.ModelSerializer):
     class Meta:
         model = InnovativeWorkReport
         fields = '__all__'
+        
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['employee_id'] = EmployeeSerializer(instance.employee_id).data
+        data['category'] = InnovativeWorkCategorySerializer(instance.category).data
+        return data
         
 class HumanDevelopmentReportSerializers(serializers.ModelSerializer):
     class Meta:
         model = HumanDevelopmentReport
         fields = '__all__'
         
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['employee_id'] = EmployeeSerializer(instance.employee_id).data
+        return data
+        
+class MajorRoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MajorRole
+        fields = '__all__'
 class DutyReportSerializers(serializers.ModelSerializer):
     class Meta:
         model = DutyReport
         fields = '__all__'
+        
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['employee_id'] = EmployeeSerializer(instance.employee_id).data
+        data['role'] = MajorRoleSerializer(instance.role).data
+        return data
