b3ebfcabeb597c324bb9b1f9790b143fd3a49dd9
Hermawan Sentyaki <52445704+dark-hermes@users.noreply.github.com>
2022-01-13 21:30:38 +0700

    merge

--- static/js/vueApp.js
+++ static/js/vueApp.js
@@ -1,6 +1,6 @@
 dayjs.extend(window.dayjs_plugin_customParseFormat);
 
-let baseUrl = 'https://absen.smkn1cibinong.sch.id/';
+let baseUrl = 'https://localhost:8000/';
 
 function absen() {
     var app = new Vue({
