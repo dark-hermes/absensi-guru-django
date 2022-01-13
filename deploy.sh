97bbdab54b97f7c626e9e37939ca3055d0bad51a
kyuubang <bayhaqisptr04@gmail.com>
2022-01-14 06:09:49 +0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,37 @@ echo "git pull..."
 git pull
 
 # change env-dev to env-prod in absensi_guru/settings.py
-sed -i sed -i 's/.env-dev/.env-prod/g' absensi_guru/settings.py
+echo "changing settings file"
+sed -i 's/.env-dev/.env-prod/g' absensi_guru/settings.py
+sed -i 's/localhost:8000/absen.smkn1cibinong.sch.id/g' static/js/vueApp.js
+
+# collectstatic
+source prod/bin/activate
+python manage.py migrate --no-input
+if [ $? -eq 1 ];
+then
+  echo "db connection failed, please check password and user correctly"
+  exit 2
+fi
+
+python manage.py collectstatic --no-input
+if [ $? -eq 1 ];
+then
+  echo "staticfile error please check path correctly"
+  exit 2
+fi
 
 # restarting gunicorn and nginx
+gunicornrestart
+if [ $? -eq 1 ];
+then
+  echo "gunicorn restart failed please check it out!"
+fi
+
+nginxrestart
+if [ $? -eq 1 ];
+then
+  echo "nginx restart failed please check it out!"
+fi
+
 
