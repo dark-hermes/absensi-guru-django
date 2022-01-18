70e80f69a3da4c1602d77605611700320f1f2cd2
Hermawan Sentyaki <52445704+dark-hermes@users.noreply.github.com>
2022-01-18 13:49:57 +0700

    fix static for production

--- absensi_guru/settings.py
+++ absensi_guru/settings.py
@@ -133,13 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if env('HOST') == 'localhost':
-    STATIC_ROOT = os.path.join(BASE_DIR, '/static')
-    STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    Path(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
